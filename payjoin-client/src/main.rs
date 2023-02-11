4d1a8e3f065055ea52e44d2c4f906273d77c6923
DanGould <d@ngould.dev>
2023-02-11 10:15:05 -0500

    Organize commands with clap

--- payjoin-client/src/main.rs
+++ payjoin-client/src/main.rs
@@ -3,29 +3,29 @@ use std::convert::TryFrom;
 
 use bitcoincore_rpc::bitcoin::Amount;
 use bitcoincore_rpc::RpcApi;
+use clap::{arg, Command};
 use payjoin::bitcoin::util::psbt::PartiallySignedTransaction as Psbt;
 use payjoin::{PjUriExt, UriExt};
 
 fn main() {
     let mut args = std::env::args_os();
+    let matches = cli().get_matches();
     let _program_name = args.next().expect("not even program name given");
-    let port = args
-        .next()
-        .expect("Missing arguments: port cookie_file bip21")
-        .into_string()
+    let port = matches
+        .get_one::<String>("PORT")
         .expect("port is not UTF-8")
         .parse::<u16>()
         .expect("port must be a number");
 
-    let cookie_file = args.next().expect("Missing arguments: cookie_file bip21");
+    let cookie_file =
+        matches.get_one::<String>("COOKIE_FILE").expect("Missing arguments: cookie_file bip21");
 
-    let bip21 =
-        args.next().expect("Missing arguments: bip21").into_string().expect("bip21 is not UTF-8");
+    let bip21 = matches.get_one::<String>("BIP21").expect("Missing arguments: bip21");
 
     send_payjoin(bip21, port, cookie_file.into())
 }
 
-fn send_payjoin(bip21: String, port: u16, cookie_file: std::path::PathBuf) {
+fn send_payjoin(bip21: &str, port: u16, cookie_file: std::path::PathBuf) {
     let link = payjoin::Uri::try_from(bip21).unwrap();
 
     let link = link
@@ -105,4 +105,15 @@ fn serialize_psbt(psbt: &Psbt) -> String {
         encoder.finish().expect("Vec doesn't return errors in its write implementation"),
     )
     .unwrap()
+}
+
+fn cli() -> Command {
+    Command::new("payjoin")
+        .about("Transfer bitcoin and preserve your privacy")
+        .arg(arg!(<PORT> "The port of the bitcoin node"))
+        .arg_required_else_help(true)
+        .arg(arg!(<COOKIE_FILE> "Path to the cookie file of the bitcoin node"))
+        .arg_required_else_help(true)
+        .arg(arg!(<BIP21> "The `bitcoin:...` payjoin uri to send to"))
+        .arg_required_else_help(true)
 }

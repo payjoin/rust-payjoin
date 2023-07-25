e5b40f0fb2db593a3a401db47b83ec6105440fbe
BitcoinZavior <BitcoinZavior@GMail.Com>
2023-07-25 19:00:15 -0400

    code clean up

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,28 +1,20 @@
 use std::{ sync::Mutex, str::FromStr };
 use bitcoin::blockdata::transaction::Transaction as BitcoinTransaction;
-use bitcoin::psbt;
+use bitcoin::psbt::PartiallySignedTransaction as BitcoinPsbt;
 
 use crate::error::Error;
 
 pub struct PartiallySignedTransaction {
-    pub(crate) internal: Mutex<psbt::PartiallySignedTransaction>,
+    pub(crate) internal: Mutex<BitcoinPsbt>,
 }
 impl PartiallySignedTransaction {
     pub(crate) fn new(psbt_base64: String) -> Result<Self, Error> {
-        let psbt: psbt::PartiallySignedTransaction = psbt::PartiallySignedTransaction::from_str(
-            &psbt_base64
-        )?;
+        let psbt: BitcoinPsbt = BitcoinPsbt::from_str(&psbt_base64)?;
         Ok(PartiallySignedTransaction {
             internal: Mutex::new(psbt),
         })
     }
 }
 pub struct Transaction {
-    pub(crate) internal: BitcoinTransaction,
+    pub(crate) internal: Mutex<BitcoinTransaction>,
 }
-
-// impl From<Transaction> for BitcoinTransaction {
-//     fn from(value: Transaction) -> Self {
-//         BitcoinTransaction::
-//     }
-// }

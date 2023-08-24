91c8b337f68aee1b450a651248f43a001b55b93a
BitcoinZavior <BitcoinZavior@GMail.Com>
2023-08-23 23:37:54 -0400

    Encapsulate internal variable inside Configuration with Option

--- src/send.rs
+++ src/send.rs
@@ -1,7 +1,4 @@
-use std::{
-	string::ParseError,
-	sync::{Arc, Mutex, RwLock},
-};
+use std::{string::ParseError, sync::RwLock};
 
 pub use payjoin::send::{Configuration as PdkConfiguration, Context as PdkContext};
 
@@ -9,7 +6,7 @@ pub use payjoin::send::{Configuration as PdkConfiguration, Context as PdkContext
 ///These parameters define how client wants to handle Payjoin.
 
 pub(crate) struct Configuration {
-	pub(crate) internal: RwLock<PdkConfiguration>,
+	pub(crate) internal: RwLock<Option<PdkConfiguration>>,
 }
 
 impl Configuration {
@@ -17,16 +14,16 @@ impl Configuration {
 	///These parameters will allow the receiver to take max_fee_contribution from given change output to pay for additional inputs. The recommended fee is size_of_one_input * fee_rate.
 	///change_index specifies which output can be used to pay fee. If None is provided, then the output is auto-detected unless the supplied transaction has more than two outputs.
 	pub fn with_fee_contribution(max_fee_contribution: u64, change_index: Option<usize>) -> Self {
-		let conf = PdkConfiguration::with_fee_contribution(
+		let configuration = PdkConfiguration::with_fee_contribution(
 			bitcoin::Amount::from_sat(max_fee_contribution),
 			change_index,
 		);
-		Self { internal: RwLock::new(conf) }
+		Self { internal: RwLock::new(Some(configuration)) }
 	}
 	///Perform Payjoin without incentivizing the payee to cooperate.
 	///While it’s generally better to offer some contribution some users may wish not to. This function disables contribution.
-	pub fn non_incentivizing() -> Arc<Self> {
-		Arc::new(Self { internal: RwLock::new(PdkConfiguration::non_incentivizing()) })
+	pub fn non_incentivizing() -> Self {
+		Self { internal: RwLock::new(Some(PdkConfiguration::non_incentivizing())) }
 	}
 	///Disable output substitution even if the receiver didn’t.
 	///This forbids receiver switching output or decreasing amount. It is generally not recommended to set this as it may prevent the receiver from doing advanced operations such as opening LN channels and it also guarantees the receiver will not reward the sender with a discount.
@@ -34,26 +31,19 @@ impl Configuration {
 		{
 			let mut data_guard = self.internal.write().unwrap();
 			// Temporarily take out the Configuration and replace with a dummy value
-			let taken_config = std::mem::replace(
-				&mut *data_guard,
-				PdkConfiguration::with_fee_contribution(bitcoin::Amount::ZERO, None),
-			);
-			*data_guard = taken_config.always_disable_output_substitution(disable);
+			let _config = std::mem::replace(&mut *data_guard, None);
+			*data_guard = Some(_config.unwrap().always_disable_output_substitution(disable));
 		}
 		self
 	}
 	///Decrease fee contribution instead of erroring.
-
 	///If this option is set and a transaction with change amount lower than fee contribution is provided then instead of returning error the fee contribution will be just lowered to match the change amount.
-	pub fn clamp_fee_contribution(mut self, clamp: bool) -> Self {
+	pub fn clamp_fee_contribution(self, clamp: bool) -> Self {
 		{
 			let mut data_guard = self.internal.write().unwrap();
 			// Temporarily take out the Configuration and replace with a dummy value
-			let taken_config = std::mem::replace(
-				&mut *data_guard,
-				PdkConfiguration::with_fee_contribution(bitcoin::Amount::ZERO, None),
-			);
-			*data_guard = taken_config.clamp_fee_contribution(clamp);
+			let _config = std::mem::replace(&mut *data_guard, None);
+			*data_guard = Some(_config.unwrap().clamp_fee_contribution(clamp));
 		}
 		self
 	}
@@ -62,11 +52,8 @@ impl Configuration {
 		{
 			let mut data_guard = self.internal.write().unwrap();
 			// Temporarily take out the Configuration and replace with a dummy value
-			let taken_config = std::mem::replace(
-				&mut *data_guard,
-				PdkConfiguration::with_fee_contribution(bitcoin::Amount::ZERO, None),
-			);
-			*data_guard = taken_config.min_fee_rate_sat_per_vb(fee_rate);
+			let _config = std::mem::replace(&mut *data_guard, None);
+			*data_guard = Some(_config.unwrap().min_fee_rate_sat_per_vb(fee_rate));
 		}
 		self
 	}

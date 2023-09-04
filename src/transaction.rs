6c0a89bf17c677a7cb24500a590885c9cdf68254
BitcoinZavior <BitcoinZavior@GMail.Com>
2023-09-03 20:51:30 -0400

    Added comments considering autogen docs

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,15 +1,16 @@
-use bitcoin::blockdata::transaction::Transaction as BitcoinTransaction;
+use payjoin::bitcoin::blockdata::transaction::Transaction as BitcoinTransaction;
 use payjoin::bitcoin::psbt::PartiallySignedTransaction as BitcoinPsbt;
 use std::{str::FromStr, sync::Arc};
 
-use crate::error::Error;
+///
+/// Partially signed transaction, commonly referred to as a PSBT.
 #[derive(Debug, Clone)]
 pub struct PartiallySignedTransaction {
 	pub internal: Arc<BitcoinPsbt>,
 }
 impl PartiallySignedTransaction {
-	pub fn new(psbt_base64: String) -> Result<Self, Error> {
-		let psbt: BitcoinPsbt = BitcoinPsbt::from_str(&psbt_base64)?;
+	pub fn new(psbt_base64: String) -> Result<Self, anyhow::Error> {
+		let psbt = BitcoinPsbt::from_str(&psbt_base64)?;
 		Ok(PartiallySignedTransaction { internal: Arc::new(psbt) })
 	}
 	pub fn serialize(&self) -> Vec<u8> {

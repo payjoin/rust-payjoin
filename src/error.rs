4c672bbc231c2c9f1b3e3c6a51c84e3609f4f4e6
BitcoinZavior <BitcoinZavior@GMail.Com>
2023-09-21 20:44:28 -0400

    enum renamed to PayjoinError

--- src/error.rs
+++ src/error.rs
@@ -1,49 +1,58 @@
 use payjoin::bitcoin::psbt::PsbtParseError;
 
 #[derive(Debug, PartialEq, Eq, thiserror::Error)]
-pub enum Error {
-	#[error("Error while parsing the string: {0}")]
-	InvalidAddress(String),
+pub enum PayjoinError {
+	#[error("Error while parsing the string: {message} ")]
+	InvalidAddress { message: String },
+	#[error("Error while parsing the script: {message}")]
+	InvalidScript { message: String },
 
-	#[error("Invalid script: {0}")]
-	InvalidScript(String),
+	#[error("Error encountered while decoding PSBT: {message} ")]
+	PsbtParseError { message: String },
 
-	#[error("Error encountered while decoding PSBT : {0}")]
-	PsbtParseError(String),
-
-	#[error("Receive error : {0}")]
-	ReceiveError(String),
+	#[error("Receive error: {message}")]
+	ReceiveError { message: String },
 
 	///Error that may occur when the request from sender is malformed.
 	///This is currently opaque type because we aren’t sure which variants will stay. You can only display it.
-	#[error("Error encountered while processing the sender's request : {0}")]
-	RequestError(String),
+	#[error("Error encountered while processing the sender's request : {message}")]
+	RequestError { message: String },
 
-	// ///Error that may occur when the request from sender is malformed.
-	// #[error("Error encountered while decoding tranaction data : {0}")]
-	// TransactionError(String),
+	///Error that may occur when the request from sender is malformed.
+	#[error("Error encountered while decoding tranaction data : {message}")]
+	TransactionError { message: String },
+	// To be returned as HTTP 500
+	#[error("HTTP 500 : {message}")]
+	ServerError { message: String },
+
 	///Error that may occur when coin selection fails.
-	#[error("Selection error : {0}")]
-	SelectionError(String),
+	#[error("Error that may occur when coin selection fails.")]
+	SelectionError,
 
 	///Error returned when request could not be created.
 	///This error can currently only happen due to programmer mistake.
-	#[error("Error creating the request: {0}")]
-	CreateRequestError(String),
+	#[error("Error creating the request: {message}")]
+	CreateRequestError { message: String },
 
-	#[error("Error parsing the Pj URL: {0}")]
-	PjParseError(String),
+	#[error("Error parsing the Pj URL:: {message}")]
+	PjParseError { message: String },
 
-	#[error("{0}")]
-	PjNotSupported(String),
+	#[error("{message}")]
+	PjNotSupported { message: String },
 
-	// #[error("Malformed response from receiver is : {0}")] ContextValidationError(String),
-	#[error("Unexpected error occured: {0}")]
-	UnexpectedError(String),
+	#[error("Malformed response from receiver is : {message}")]
+	ContextValidationError { message: String },
+	#[error("Unexpected error occured: {message}")]
+	UnexpectedError { message: String },
 }
 
-impl From<PsbtParseError> for Error {
+impl From<PsbtParseError> for PayjoinError {
 	fn from(value: PsbtParseError) -> Self {
-		Error::PsbtParseError(value.to_string())
+		PayjoinError::PsbtParseError { message: value.to_string() }
+	}
+}
+impl From<uniffi::UnexpectedUniFFICallbackError> for PayjoinError {
+	fn from(e: uniffi::UnexpectedUniFFICallbackError) -> Self {
+		Self::UnexpectedError { message: e.reason }
 	}
 }

c3e6521011e3888c093a48f03bc2be577c7f6b6d
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-01-14 13:07:00 -0500

    dependency upgraded

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 use bitcoincore_rpc::bitcoin::block::ValidationError;
 use payjoin::bitcoin::psbt::PsbtParseError;
-use payjoin::receive::RequestError;
+use payjoin::receive::{RequestError, SelectionError};
 use payjoin::send::{CreateRequestError, ResponseError as PdkResponseError};
 use payjoin::Error;
 use std::fmt::Debug;
@@ -26,7 +26,7 @@ pub enum PayjoinError {
 	RequestError { message: String },
 
 	///Error that may occur when the request from sender is malformed.
-	#[error("Error encountered while decoding tranaction data : {message}")]
+	#[error("Error encountered while decoding transaction data : {message}")]
 	TransactionError { message: String },
 	// To be returned as HTTP 500
 	#[error("HTTP 500 : {message}")]
@@ -55,8 +55,73 @@ pub enum PayjoinError {
 
 	#[error("Unexpected error occurred: {message}")]
 	UnexpectedError { message: String },
+
+	#[error("{message}")]
+	OhttpError { message: String },
 }
 
+// #[derive(Debug, PartialEq, Eq, thiserror::Error)]
+// pub enum OhttpError{
+// 	#[error("{message}")]
+// 	AeadError{ message: String },
+//
+// 	#[error("{message}")]
+// 	CryptoError{ message: String },
+//
+// 	#[error("Error was found in the format")]
+// 	FormatError,
+//
+// 	#[error("{message}")]
+// 	HpkeError{ message: String },
+//
+// 	#[error("An internal error occurred")]
+// 	InternalError,
+//
+// 	#[error("The wrong type of key was provided for the selected KEM")]
+// 	InvalidKeyTypeError,
+//
+// 	#[error("The wrong KEM was specified")]
+// 	InvalidKemError,
+//
+// 	#[error("{message}")]
+// 	IoError{ message: String },
+//
+// 	#[error("The key ID was invalid")]
+// 	KeyIdError,
+//
+// 	#[error("A field was truncated")]
+// 	TruncatedError,
+//
+// 	#[error("The configuration was not supported")]
+// 	UnsupportedError,
+//
+// 	#[error("The configuration contained too many symmetric suites")]
+// 	TooManySymmetricSuitesError,
+// }
+
+// impl From<ohttp::Error> for OhttpError{
+// 	fn from(value: ohttp::Error) -> Self {
+// 		match value {
+// 			ohttp::Error::Aead(e) => OhttpError::AeadError{message:e.to_string()},
+// 			ohttp::Error::Format =>  OhttpError::FormatError,
+// 			ohttp::Error::Internal => OhttpError::InternalError,
+// 			ohttp::Error::InvalidKeyType => OhttpError::InvalidKeyTypeError,
+// 			ohttp::Error::InvalidKem => OhttpError::InvalidKemError,
+// 			ohttp::Error::Io(e) => OhttpError::IoError{message:e.to_string()},
+// 			ohttp::Error::KeyId => OhttpError::KeyIdError,
+// 			ohttp::Error::Truncated => OhttpError::TruncatedError,
+// 			ohttp::Error::Unsupported => OhttpError::UnsupportedError,
+// 			ohttp::Error::TooManySymmetricSuites => OhttpError::TooManySymmetricSuitesError,
+// 			ohttp::Error::Hpke(e) => OhttpError::HpkeError{message:e.to_string()}
+// 		}
+// 	}
+// }
+
+impl From<ohttp::Error> for PayjoinError {
+	fn from(value: ohttp::Error) -> Self {
+		PayjoinError::OhttpError { message: value.to_string() }
+	}
+}
 impl From<PsbtParseError> for PayjoinError {
 	fn from(value: PsbtParseError) -> Self {
 		PayjoinError::PsbtParseError { message: value.to_string() }
@@ -68,7 +133,7 @@ impl From<Error> for PayjoinError {
 		match value {
 			Error::BadRequest(e) => e.into(),
 			Error::Server(e) => PayjoinError::ServerError { message: e.to_string() },
-			Error::V2(e) => PayjoinError::V2Error {message: format!("{:?}", e), }
+			Error::V2(e) => PayjoinError::V2Error { message: format!("{:?}", e) },
 		}
 	}
 }
@@ -78,7 +143,11 @@ impl From<RequestError> for PayjoinError {
 		PayjoinError::RequestError { message: value.to_string() }
 	}
 }
-
+impl From<SelectionError> for PayjoinError {
+	fn from(value: SelectionError) -> Self {
+		PayjoinError::SelectionError { message: format!("{:?}", value) }
+	}
+}
 impl From<PdkResponseError> for PayjoinError {
 	fn from(value: PdkResponseError) -> Self {
 		PayjoinError::ResponseError { message: value.to_string() }

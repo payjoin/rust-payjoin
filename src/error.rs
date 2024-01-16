bd68d86190b3fe48c8f8f7d50e92827fd92f0cd1
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-01-16 17:34:00 -0500

    code formatted

--- src/error.rs
+++ src/error.rs
@@ -1,63 +1,64 @@
+use std::fmt::Debug;
+
 use bitcoincore_rpc::bitcoin::block::ValidationError;
 use payjoin::bitcoin::psbt::PsbtParseError;
 use payjoin::receive::{RequestError, SelectionError};
 use payjoin::send::{CreateRequestError, ResponseError as PdkResponseError};
 use payjoin::Error;
-use std::fmt::Debug;
 
 #[derive(Debug, PartialEq, Eq, thiserror::Error)]
 pub enum PayjoinError {
-	#[error("Error while parsing the string: {message} ")]
-	InvalidAddress { message: String },
-	#[error("Error while parsing the script: {message}")]
-	InvalidScript { message: String },
+    #[error("Error while parsing the string: {message} ")]
+    InvalidAddress { message: String },
+    #[error("Error while parsing the script: {message}")]
+    InvalidScript { message: String },
 
-	#[error("{message}")]
-	NetworkValidation { message: String },
-	#[error("Error encountered while decoding PSBT: {message} ")]
-	PsbtParseError { message: String },
+    #[error("{message}")]
+    NetworkValidation { message: String },
+    #[error("Error encountered while decoding PSBT: {message} ")]
+    PsbtParseError { message: String },
 
-	#[error("Response error: {message}")]
-	ResponseError { message: String },
+    #[error("Response error: {message}")]
+    ResponseError { message: String },
 
-	///Error that may occur when the request from sender is malformed.
-	///This is currently opaque type because we aren’t sure which variants will stay. You can only display it.
-	#[error("Error encountered while processing the sender's request : {message}")]
-	RequestError { message: String },
+    ///Error that may occur when the request from sender is malformed.
+    ///This is currently opaque type because we aren’t sure which variants will stay. You can only display it.
+    #[error("Error encountered while processing the sender's request : {message}")]
+    RequestError { message: String },
 
-	///Error that may occur when the request from sender is malformed.
-	#[error("Error encountered while decoding transaction data : {message}")]
-	TransactionError { message: String },
-	// To be returned as HTTP 500
-	#[error("HTTP 500 : {message}")]
-	ServerError { message: String },
+    ///Error that may occur when the request from sender is malformed.
+    #[error("Error encountered while decoding transaction data : {message}")]
+    TransactionError { message: String },
+    // To be returned as HTTP 500
+    #[error("HTTP 500 : {message}")]
+    ServerError { message: String },
 
-	///Error that may occur when coin selection fails.
-	#[error("Error that may occur when coin selection fails: {message}")]
-	SelectionError { message: String },
+    ///Error that may occur when coin selection fails.
+    #[error("Error that may occur when coin selection fails: {message}")]
+    SelectionError { message: String },
 
-	///Error returned when request could not be created.
-	///This error can currently only happen due to programmer mistake.
-	#[error("Error creating the request: {message}")]
-	CreateRequestError { message: String },
+    ///Error returned when request could not be created.
+    ///This error can currently only happen due to programmer mistake.
+    #[error("Error creating the request: {message}")]
+    CreateRequestError { message: String },
 
-	#[error("Error parsing the Pj URL: {message}")]
-	PjParseError { message: String },
+    #[error("Error parsing the Pj URL: {message}")]
+    PjParseError { message: String },
 
-	#[error("{message}")]
-	PjNotSupported { message: String },
+    #[error("{message}")]
+    PjNotSupported { message: String },
 
-	#[error("Malformed response from receiver is : {message}")]
-	ValidationError { message: String },
+    #[error("Malformed response from receiver is : {message}")]
+    ValidationError { message: String },
 
-	#[error("V2Error: {message}")]
-	V2Error { message: String },
+    #[error("V2Error: {message}")]
+    V2Error { message: String },
 
-	#[error("Unexpected error occurred: {message}")]
-	UnexpectedError { message: String },
+    #[error("Unexpected error occurred: {message}")]
+    UnexpectedError { message: String },
 
-	#[error("{message}")]
-	OhttpError { message: String },
+    #[error("{message}")]
+    OhttpError { message: String },
 }
 
 // #[derive(Debug, PartialEq, Eq, thiserror::Error)]
@@ -118,53 +119,53 @@ pub enum PayjoinError {
 // }
 
 impl From<ohttp::Error> for PayjoinError {
-	fn from(value: ohttp::Error) -> Self {
-		PayjoinError::OhttpError { message: value.to_string() }
-	}
+    fn from(value: ohttp::Error) -> Self {
+        PayjoinError::OhttpError { message: value.to_string() }
+    }
 }
 impl From<PsbtParseError> for PayjoinError {
-	fn from(value: PsbtParseError) -> Self {
-		PayjoinError::PsbtParseError { message: value.to_string() }
-	}
+    fn from(value: PsbtParseError) -> Self {
+        PayjoinError::PsbtParseError { message: value.to_string() }
+    }
 }
 
 impl From<Error> for PayjoinError {
-	fn from(value: Error) -> Self {
-		match value {
-			Error::BadRequest(e) => e.into(),
-			Error::Server(e) => PayjoinError::ServerError { message: e.to_string() },
-			Error::V2(e) => PayjoinError::V2Error { message: format!("{:?}", e) },
-		}
-	}
+    fn from(value: Error) -> Self {
+        match value {
+            Error::BadRequest(e) => e.into(),
+            Error::Server(e) => PayjoinError::ServerError { message: e.to_string() },
+            Error::V2(e) => PayjoinError::V2Error { message: format!("{:?}", e) },
+        }
+    }
 }
 
 impl From<RequestError> for PayjoinError {
-	fn from(value: RequestError) -> Self {
-		PayjoinError::RequestError { message: value.to_string() }
-	}
+    fn from(value: RequestError) -> Self {
+        PayjoinError::RequestError { message: value.to_string() }
+    }
 }
 impl From<SelectionError> for PayjoinError {
-	fn from(value: SelectionError) -> Self {
-		PayjoinError::SelectionError { message: format!("{:?}", value) }
-	}
+    fn from(value: SelectionError) -> Self {
+        PayjoinError::SelectionError { message: format!("{:?}", value) }
+    }
 }
 impl From<PdkResponseError> for PayjoinError {
-	fn from(value: PdkResponseError) -> Self {
-		PayjoinError::ResponseError { message: value.to_string() }
-	}
+    fn from(value: PdkResponseError) -> Self {
+        PayjoinError::ResponseError { message: value.to_string() }
+    }
 }
 impl From<ValidationError> for PayjoinError {
-	fn from(value: ValidationError) -> Self {
-		PayjoinError::ValidationError { message: value.to_string() }
-	}
+    fn from(value: ValidationError) -> Self {
+        PayjoinError::ValidationError { message: value.to_string() }
+    }
 }
 impl From<CreateRequestError> for PayjoinError {
-	fn from(value: CreateRequestError) -> Self {
-		PayjoinError::CreateRequestError { message: value.to_string() }
-	}
+    fn from(value: CreateRequestError) -> Self {
+        PayjoinError::CreateRequestError { message: value.to_string() }
+    }
 }
 impl From<uniffi::UnexpectedUniFFICallbackError> for PayjoinError {
-	fn from(e: uniffi::UnexpectedUniFFICallbackError) -> Self {
-		Self::UnexpectedError { message: e.reason }
-	}
+    fn from(e: uniffi::UnexpectedUniFFICallbackError) -> Self {
+        Self::UnexpectedError { message: e.reason }
+    }
 }
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,9 +1,10 @@
-use std::{io::Cursor, str::FromStr, sync::Arc};
+use std::io::Cursor;
+use std::str::FromStr;
+use std::sync::Arc;
 
+use payjoin::bitcoin::blockdata::transaction::Transaction as BitcoinTransaction;
+use payjoin::bitcoin::consensus::Decodable;
 use payjoin::bitcoin::psbt::PartiallySignedTransaction as BitcoinPsbt;
-use payjoin::bitcoin::{
-	blockdata::transaction::Transaction as BitcoinTransaction, consensus::Decodable,
-};
 
 use crate::error::PayjoinError;
 
@@ -13,72 +14,72 @@ use crate::error::PayjoinError;
 pub struct PartiallySignedTransaction(BitcoinPsbt);
 
 impl From<BitcoinPsbt> for PartiallySignedTransaction {
-	fn from(value: BitcoinPsbt) -> Self {
-		PartiallySignedTransaction(value)
-	}
+    fn from(value: BitcoinPsbt) -> Self {
+        PartiallySignedTransaction(value)
+    }
 }
 
 impl From<PartiallySignedTransaction> for BitcoinPsbt {
-	fn from(value: PartiallySignedTransaction) -> Self {
-		value.0
-	}
+    fn from(value: PartiallySignedTransaction) -> Self {
+        value.0
+    }
 }
 
 impl PartiallySignedTransaction {
-	pub fn from_string(psbt_base64: String) -> Result<Self, PayjoinError> {
-		let psbt = BitcoinPsbt::from_str(&psbt_base64)?;
-		Ok(PartiallySignedTransaction(psbt))
-	}
+    pub fn from_string(psbt_base64: String) -> Result<Self, PayjoinError> {
+        let psbt = BitcoinPsbt::from_str(&psbt_base64)?;
+        Ok(PartiallySignedTransaction(psbt))
+    }
 
-	pub fn serialize(&self) -> Vec<u8> {
-		self.0.serialize()
-	}
+    pub fn serialize(&self) -> Vec<u8> {
+        self.0.serialize()
+    }
 
-	pub fn extract_tx(&self) -> Arc<Transaction> {
-		Arc::new(self.0.clone().extract_tx().into())
-	}
-	pub fn as_string(&self) -> String {
-		self.0.to_string()
-	}
+    pub fn extract_tx(&self) -> Arc<Transaction> {
+        Arc::new(self.0.clone().extract_tx().into())
+    }
+    pub fn as_string(&self) -> String {
+        self.0.to_string()
+    }
 }
 
 #[derive(Clone)]
 pub struct Transaction {
-	internal: BitcoinTransaction,
+    internal: BitcoinTransaction,
 }
 
 impl Transaction {
-	pub fn new(transaction_bytes: Vec<u8>) -> Result<Self, PayjoinError> {
-		let mut decoder = Cursor::new(transaction_bytes);
-		match BitcoinTransaction::consensus_decode(&mut decoder) {
-			Ok(e) => Ok(e.into()),
-			Err(e) => Err(PayjoinError::TransactionError { message: e.to_string() }),
-		}
-	}
-	pub fn txid(&self) -> Arc<Txid> {
-		Arc::new(Txid(self.internal.txid().to_string()))
-	}
-	pub fn serialize(&self) -> Vec<u8> {
-		payjoin::bitcoin::consensus::serialize(&self.internal)
-	}
+    pub fn new(transaction_bytes: Vec<u8>) -> Result<Self, PayjoinError> {
+        let mut decoder = Cursor::new(transaction_bytes);
+        match BitcoinTransaction::consensus_decode(&mut decoder) {
+            Ok(e) => Ok(e.into()),
+            Err(e) => Err(PayjoinError::TransactionError { message: e.to_string() }),
+        }
+    }
+    pub fn txid(&self) -> Arc<Txid> {
+        Arc::new(Txid(self.internal.txid().to_string()))
+    }
+    pub fn serialize(&self) -> Vec<u8> {
+        payjoin::bitcoin::consensus::serialize(&self.internal)
+    }
 }
 
 impl From<Transaction> for BitcoinTransaction {
-	fn from(value: Transaction) -> Self {
-		value.internal
-	}
+    fn from(value: Transaction) -> Self {
+        value.internal
+    }
 }
 
 impl From<BitcoinTransaction> for Transaction {
-	fn from(value: BitcoinTransaction) -> Self {
-		Self { internal: value }
-	}
+    fn from(value: BitcoinTransaction) -> Self {
+        Self { internal: value }
+    }
 }
 
 pub struct Txid(String);
 
 impl Txid {
-	pub fn as_string(&self) -> String {
-		self.0.clone()
-	}
+    pub fn as_string(&self) -> String {
+        self.0.clone()
+    }
 }

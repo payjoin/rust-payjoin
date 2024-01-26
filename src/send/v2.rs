9450c63fdda839bb096f56e822ec7ffe91e0f633
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-01-26 08:24:00 -0500

    cargo fmt applied

---
+++ src/send/v2.rs
@@ -0,0 +1,35 @@
+use std::io::Cursor;
+use std::sync::{Arc, Mutex};
+
+use crate::error::PayjoinError;
+use crate::transaction::PartiallySignedTransaction;
+
+pub struct ContextV2(Mutex<Option<payjoin::send::ContextV2>>);
+impl From<&ContextV2> for payjoin::send::ContextV2 {
+    fn from(value: &ContextV2) -> Self {
+        let mut data_guard = value.0.lock().unwrap();
+        Option::take(&mut *data_guard).expect("ContextV2 moved out of memory")
+    }
+}
+impl From<payjoin::send::ContextV2> for ContextV2 {
+    fn from(value: payjoin::send::ContextV2) -> Self {
+        Self(Mutex::new(Some(value)))
+    }
+}
+impl ContextV2 {
+    ///Decodes and validates the response.
+    /// Call this method with response from receiver to continue BIP-??? flow. A successful response can either be None if the relay has not response yet or Some(Psbt).
+    /// If the response is some valid PSBT you should sign and broadcast.
+    pub fn process_response(
+        &self,
+        response: Vec<u8>,
+    ) -> Result<Option<Arc<PartiallySignedTransaction>>, PayjoinError> {
+        let mut decoder = Cursor::new(response);
+        match <&ContextV2 as Into<payjoin::send::ContextV2>>::into(self)
+            .process_response(&mut decoder)
+        {
+            Ok(e) => Ok(e.map(|x| Arc::new(x.into()))),
+            Err(e) => Err(e.into()),
+        }
+    }
+}

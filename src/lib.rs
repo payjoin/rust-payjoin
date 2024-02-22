77a1d1e871dba0d735f0e4abb5b845d1419b7d73
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-02-22 04:35:00 -0500

    code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,21 @@ pub mod types;
 pub mod uri;
 
 use crate::error::PayjoinError;
+use crate::receive::v1::{
+    CanBroadcast, Headers, IsOutputKnown, IsScriptOwned, MaybeInputsOwned, MaybeInputsSeen,
+    MaybeMixedInputScripts, OutputsUnknown, PayjoinProposal, ProcessPartiallySignedTransaction,
+    ProvisionalProposal, UncheckedProposal,
+};
 #[allow(unused_imports)]
 use crate::receive::v2::{
     ClientResponse, Enrolled, Enroller, RequestResponse, V2MaybeInputsOwned, V2MaybeInputsSeen,
     V2MaybeMixedInputScripts, V2OutputsUnknown, V2PayjoinProposal, V2ProvisionalProposal,
     V2UncheckedProposal,
 };
-use crate::receive::v1::{
-    CanBroadcast, Headers, IsOutputKnown, IsScriptOwned, MaybeInputsOwned, MaybeInputsSeen,
-    MaybeMixedInputScripts, OutputsUnknown, PayjoinProposal, ProcessPartiallySignedTransaction,
-    ProvisionalProposal, UncheckedProposal,
+use crate::send::v1::{
+    ContextV1, RequestBuilder, RequestContext, RequestContextV1, RequestContextV2,
 };
 use crate::send::v2::ContextV2;
-use crate::send::v1::{ContextV1, RequestBuilder, RequestContext, RequestContextV1, RequestContextV2};
 use crate::types::{Network, OutPoint, Request, TxOut};
 use crate::uri::{PjUri, Uri, Url};
 

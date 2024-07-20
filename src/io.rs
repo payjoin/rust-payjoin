8c02096df2ab333bf967f74cd09c001dd4b7852c
kumulynja <b@kumuly.dev>
2024-07-20 15:28:02 +0200

    add feature to enable on default disabled danger-local-https

--- src/io.rs
+++ src/io.rs
@@ -16,10 +16,11 @@ use crate::uri::Url;
 pub async fn fetch_ohttp_keys(
     ohttp_relay: Url,
     payjoin_directory: Url,
-    cert_der: Vec<u8>,
+    #[cfg(feature = "enable-danger-local-https")] cert_der: Vec<u8>,
 ) -> Result<OhttpKeys, PayjoinError> {
-    payjoin::io::fetch_ohttp_keys(ohttp_relay.into(), payjoin_directory.into(), cert_der)
-        .await
-        .map(|e| e.into())
-        .map_err(|e| e.into())
+    #[cfg(not(feature = "enable-danger-local-https"))]
+    let res = payjoin::io::fetch_ohttp_keys(ohttp_relay.into(), payjoin_directory.into());
+    #[cfg(feature = "enable-danger-local-https")]
+    let res = payjoin::io::fetch_ohttp_keys(ohttp_relay.into(), payjoin_directory.into(), cert_der);
+    res.await.map(|e| e.into()).map_err(|e| e.into())
 }

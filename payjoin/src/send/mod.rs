0c527547176002ba57a4267ad6847b1d7c951091
spacebear <git@spacebear.dev>
2024-08-10 16:39:57 -0400

    Fix sender output checks
    `check_outputs` was incorrectly iterating over the proposal outputs,
    effectively skipping all output checks. This commit fixes that and
    ensures the offical test vectors pass.

--- payjoin/src/send/mod.rs
+++ payjoin/src/send/mod.rs
@@ -887,7 +887,8 @@ impl ContextV1 {
     }
 
     fn check_outputs(&self, proposal: &Psbt) -> InternalResult<OutputStats> {
-        let mut original_outputs = proposal.unsigned_tx.output.iter().enumerate().peekable();
+        let mut original_outputs =
+            self.original_psbt.unsigned_tx.output.iter().enumerate().peekable();
         let mut total_value = bitcoin::Amount::ZERO;
         let mut contributed_fee = bitcoin::Amount::ZERO;
         let mut total_weight = Weight::ZERO;
@@ -909,7 +910,7 @@ impl ContextV1 {
                     if proposed_txout.value < original_output.value {
                         contributed_fee =
                             bitcoin::Amount::from_sat(original_output.value - proposed_txout.value);
-                        ensure!(contributed_fee < max_fee_contrib, FeeContributionExceedsMaximum);
+                        ensure!(contributed_fee <= max_fee_contrib, FeeContributionExceedsMaximum);
                         //The remaining fee checks are done in the caller
                     }
                     original_outputs.next();
@@ -1142,7 +1143,7 @@ mod test {
         let ctx = super::ContextV1 {
             original_psbt,
             disable_output_substitution: false,
-            fee_contribution: None,
+            fee_contribution: Some((bitcoin::Amount::from_sat(182), 0)),
             min_fee_rate: FeeRate::ZERO,
             payee,
             input_type: InputType::SegWitV0 { ty: SegWitV0Type::Pubkey, nested: true },

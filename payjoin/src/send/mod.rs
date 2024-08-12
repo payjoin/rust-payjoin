12701f4460d65ff3e58f0131c49c7d77bdce1b1a
spacebear <git@spacebear.dev>
2024-08-12 12:04:17 -0400

    Test attempt to steal sender change
    This adds a test that attempts to steal change from the sender output
    and add that amount to the receiver output, and is expected to panic.

--- payjoin/src/send/mod.rs
+++ payjoin/src/send/mod.rs
@@ -1125,14 +1125,17 @@ fn serialize_url(
 
 #[cfg(test)]
 mod test {
+    use std::str::FromStr;
+
+    use bitcoin::psbt::PartiallySignedTransaction as Psbt;
+
+    use crate::psbt::PsbtExt;
+
     const ORIGINAL_PSBT: &str = "cHNidP8BAHMCAAAAAY8nutGgJdyYGXWiBEb45Hoe9lWGbkxh/6bNiOJdCDuDAAAAAAD+////AtyVuAUAAAAAF6kUHehJ8GnSdBUOOv6ujXLrWmsJRDCHgIQeAAAAAAAXqRR3QJbbz0hnQ8IvQ0fptGn+votneofTAAAAAAEBIKgb1wUAAAAAF6kU3k4ekGHKWRNbA1rV5tR5kEVDVNCHAQcXFgAUx4pFclNVgo1WWAdN1SYNX8tphTABCGsCRzBEAiB8Q+A6dep+Rz92vhy26lT0AjZn4PRLi8Bf9qoB/CMk0wIgP/Rj2PWZ3gEjUkTlhDRNAQ0gXwTO7t9n+V14pZ6oljUBIQMVmsAaoNWHVMS02LfTSe0e388LNitPa1UQZyOihY+FFgABABYAFEb2Giu6c4KO5YW0pfw3lGp9jMUUAAA=";
 
     const PAYJOIN_PROPOSAL: &str = "cHNidP8BAJwCAAAAAo8nutGgJdyYGXWiBEb45Hoe9lWGbkxh/6bNiOJdCDuDAAAAAAD+////jye60aAl3JgZdaIERvjkeh72VYZuTGH/ps2I4l0IO4MBAAAAAP7///8CJpW4BQAAAAAXqRQd6EnwadJ0FQ46/q6NcutaawlEMIcACT0AAAAAABepFHdAltvPSGdDwi9DR+m0af6+i2d6h9MAAAAAAQEgqBvXBQAAAAAXqRTeTh6QYcpZE1sDWtXm1HmQRUNU0IcBBBYAFMeKRXJTVYKNVlgHTdUmDV/LaYUwIgYDFZrAGqDVh1TEtNi300ntHt/PCzYrT2tVEGcjooWPhRYYSFzWUDEAAIABAACAAAAAgAEAAAAAAAAAAAEBIICEHgAAAAAAF6kUyPLL+cphRyyI5GTUazV0hF2R2NWHAQcXFgAUX4BmVeWSTJIEwtUb5TlPS/ntohABCGsCRzBEAiBnu3tA3yWlT0WBClsXXS9j69Bt+waCs9JcjWtNjtv7VgIge2VYAaBeLPDB6HGFlpqOENXMldsJezF9Gs5amvDQRDQBIQJl1jz1tBt8hNx2owTm+4Du4isx0pmdKNMNIjjaMHFfrQABABYAFEb2Giu6c4KO5YW0pfw3lGp9jMUUIgICygvBWB5prpfx61y1HDAwo37kYP3YRJBvAjtunBAur3wYSFzWUDEAAIABAACAAAAAgAEAAAABAAAAAAA=";
 
     fn create_v1_context() -> super::ContextV1 {
-        use std::str::FromStr;
-
-        use bitcoin::psbt::Psbt;
         use bitcoin::FeeRate;
 
         use crate::input_type::{InputType, SegWitV0Type};
@@ -1154,10 +1157,6 @@ mod test {
 
     #[test]
     fn official_vectors() {
-        use std::str::FromStr;
-
-        use bitcoin::psbt::Psbt;
-
         use crate::psbt::PsbtExt;
 
         let original_psbt = Psbt::from_str(ORIGINAL_PSBT).unwrap();
@@ -1176,51 +1175,23 @@ mod test {
     }
 
     #[test]
-    #[cfg(feature = "v2")]
-    fn req_ctx_ser_de_roundtrip() {
-        use super::*;
-
-        let req_ctx = RequestContext {
-            psbt: Psbt::from_str(ORIGINAL_PSBT).unwrap(),
-            endpoint: Url::parse("http://localhost:1234").unwrap(),
-            ohttp_config: None,
-            disable_output_substitution: false,
-            fee_contribution: None,
-            min_fee_rate: FeeRate::ZERO,
-            input_type: InputType::SegWitV0 {
-                ty: crate::input_type::SegWitV0Type::Pubkey,
-                nested: true,
-            },
-            sequence: Sequence::MAX,
-            payee: ScriptBuf::from(vec![0x00]),
-            e: bitcoin::secp256k1::SecretKey::from_slice(&[0x01; 32]).unwrap(),
-        };
-        let serialized = serde_json::to_string(&req_ctx).unwrap();
-        let deserialized = serde_json::from_str(&serialized).unwrap();
-        assert!(req_ctx == deserialized);
-    }
-
-    #[test]
-    fn handle_known_errors() {
+    #[should_panic]
+    fn test_receiver_steals_sender_change() {
+        let original_psbt = Psbt::from_str(ORIGINAL_PSBT).unwrap();
+        eprintln!("original: {:#?}", original_psbt);
         let ctx = create_v1_context();
-        let known_json_error = serde_json::json!({
-            "errorCode": "version-unsupported",
-            "message": "This version of payjoin is not supported."
-        })
-        .to_string();
-        assert_eq!(
-            ctx.process_response(&mut known_json_error.as_bytes()).unwrap_err().to_string(),
-            "This version of payjoin is not supported."
-        );
-        let ctx = create_v1_context();
-        let invalid_json_error = serde_json::json!({
-            "err": "random",
-            "message": "This version of payjoin is not supported."
-        })
-        .to_string();
-        assert_eq!(
-            ctx.process_response(&mut invalid_json_error.as_bytes()).unwrap_err().to_string(),
-            "The receiver sent an invalid response: couldn't decode as PSBT or JSON"
-        )
+        let mut proposal = Psbt::from_str(PAYJOIN_PROPOSAL).unwrap();
+        eprintln!("proposal: {:#?}", proposal);
+        for output in proposal.outputs_mut() {
+            output.bip32_derivation.clear();
+        }
+        for input in proposal.inputs_mut() {
+            input.bip32_derivation.clear();
+        }
+        proposal.inputs_mut()[0].witness_utxo = None;
+        // Steal 0.5 BTC from the sender output and add it to the receiver output
+        proposal.unsigned_tx.output[0].value -= 50000000;
+        proposal.unsigned_tx.output[1].value += 50000000;
+        ctx.process_proposal(proposal).unwrap();
     }
 }

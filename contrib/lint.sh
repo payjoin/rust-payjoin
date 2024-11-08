2da1be5eef173629dbe19951164cb633b0a8b78b
spacebear <git@spacebear.dev>
2024-11-08 12:03:38 -0500

    Add contrib/lint.sh
    Run linting in more places (e.g. in tests, and with v2 features
    enabled).

---
+++ contrib/lint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+cargo clippy --all-targets --keep-going --features=send,receive -- -D warnings
+cargo clippy --all-targets --keep-going --features=v2,danger-local-https,io -- -D warnings

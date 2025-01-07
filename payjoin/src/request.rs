a2f89112e6ab543b82329967f8ef08c9b8e0523b
DanGould <d@ngould.dev>
2025-01-06 23:54:13 -0500

    Make Request impl fns pub(crate)
    They're only meant to be used internally

--- payjoin/src/request.rs
+++ payjoin/src/request.rs
@@ -27,12 +27,14 @@ pub struct Request {
 }
 
 impl Request {
-    pub fn new_v1(url: Url, body: Vec<u8>) -> Self {
+    /// Construct a new v1 request.
+    pub(crate) fn new_v1(url: Url, body: Vec<u8>) -> Self {
         Self { url, content_type: V1_REQ_CONTENT_TYPE, body }
     }
 
+    /// Construct a new v2 request.
     #[cfg(feature = "v2")]
-    pub fn new_v2(url: Url, body: [u8; crate::ohttp::ENCAPSULATED_MESSAGE_BYTES]) -> Self {
+    pub(crate) fn new_v2(url: Url, body: [u8; crate::ohttp::ENCAPSULATED_MESSAGE_BYTES]) -> Self {
         Self { url, content_type: V2_REQ_CONTENT_TYPE, body: body.to_vec() }
     }
 }

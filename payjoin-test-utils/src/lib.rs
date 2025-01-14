424af98495438fc720012647297984158656df19
spacebear <git@spacebear.dev>
2025-01-14 18:45:21 -0500

    Expose payjoin-test-utils as a standalone crate
    This new crate contains internal test utilities shared between
    payjoin integration tests and payjoin-cli e2e tests.
    
    It also provides an opportunity to create downstream test
    fixtures in payjoin-ffi and language bindings downstream of it.
    
    Co-authored-by: DanGould <d@ngould.dev>

---
+++ payjoin-test-utils/src/lib.rs
@@ -0,0 +1,116 @@
+use std::env;
+use std::sync::Arc;
+use std::time::Duration;
+
+use bitcoin::Amount;
+use bitcoind::bitcoincore_rpc::json::AddressType;
+use bitcoind::bitcoincore_rpc::{self, RpcApi};
+use http::StatusCode;
+use log::{log_enabled, Level};
+use once_cell::sync::OnceCell;
+use reqwest::{Client, ClientBuilder};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+use url::Url;
+
+pub type BoxError = Box<dyn std::error::Error + 'static>;
+pub type BoxSendSyncError = Box<dyn std::error::Error + Send + Sync>;
+
+static INIT_TRACING: OnceCell<()> = OnceCell::new();
+
+pub fn init_tracing() {
+    INIT_TRACING.get_or_init(|| {
+        let subscriber = FmtSubscriber::builder()
+            .with_env_filter(EnvFilter::from_default_env())
+            .with_test_writer()
+            .finish();
+
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("failed to set global default subscriber");
+    });
+}
+
+pub async fn init_directory(
+    db_host: String,
+    local_cert_key: (Vec<u8>, Vec<u8>),
+) -> std::result::Result<
+    (u16, tokio::task::JoinHandle<std::result::Result<(), BoxSendSyncError>>),
+    BoxSendSyncError,
+> {
+    println!("Database running on {}", db_host);
+    let timeout = Duration::from_secs(2);
+    payjoin_directory::listen_tcp_with_tls_on_free_port(db_host, timeout, local_cert_key).await
+}
+
+// generates or gets a DER encoded localhost cert and key.
+pub fn local_cert_key() -> (Vec<u8>, Vec<u8>) {
+    let cert =
+        rcgen::generate_simple_self_signed(vec!["0.0.0.0".to_string(), "localhost".to_string()])
+            .expect("Failed to generate cert");
+    let cert_der = cert.serialize_der().expect("Failed to serialize cert");
+    let key_der = cert.serialize_private_key_der();
+    (cert_der, key_der)
+}
+
+pub fn init_bitcoind_sender_receiver(
+    sender_address_type: Option<AddressType>,
+    receiver_address_type: Option<AddressType>,
+) -> Result<(bitcoind::BitcoinD, bitcoincore_rpc::Client, bitcoincore_rpc::Client), BoxError> {
+    let bitcoind_exe =
+        env::var("BITCOIND_EXE").ok().or_else(|| bitcoind::downloaded_exe_path().ok()).unwrap();
+    let mut conf = bitcoind::Conf::default();
+    conf.view_stdout = log_enabled!(Level::Debug);
+    let bitcoind = bitcoind::BitcoinD::with_conf(bitcoind_exe, &conf)?;
+    let receiver = bitcoind.create_wallet("receiver")?;
+    let receiver_address = receiver.get_new_address(None, receiver_address_type)?.assume_checked();
+    let sender = bitcoind.create_wallet("sender")?;
+    let sender_address = sender.get_new_address(None, sender_address_type)?.assume_checked();
+    bitcoind.client.generate_to_address(1, &receiver_address)?;
+    bitcoind.client.generate_to_address(101, &sender_address)?;
+
+    assert_eq!(
+        Amount::from_btc(50.0)?,
+        receiver.get_balances()?.mine.trusted,
+        "receiver doesn't own bitcoin"
+    );
+
+    assert_eq!(
+        Amount::from_btc(50.0)?,
+        sender.get_balances()?.mine.trusted,
+        "sender doesn't own bitcoin"
+    );
+    Ok((bitcoind, sender, receiver))
+}
+
+pub fn http_agent(cert_der: Vec<u8>) -> Result<Client, BoxError> {
+    Ok(http_agent_builder(cert_der)?.build()?)
+}
+
+fn http_agent_builder(cert_der: Vec<u8>) -> Result<ClientBuilder, BoxError> {
+    Ok(ClientBuilder::new()
+        .danger_accept_invalid_certs(true)
+        .use_rustls_tls()
+        .add_root_certificate(reqwest::tls::Certificate::from_der(cert_der.as_slice()).unwrap()))
+}
+
+const TESTS_TIMEOUT: Duration = Duration::from_secs(20);
+const WAIT_SERVICE_INTERVAL: Duration = Duration::from_secs(3);
+
+pub async fn wait_for_service_ready(
+    service_url: Url,
+    agent: Arc<Client>,
+) -> Result<(), &'static str> {
+    let health_url = service_url.join("/health").map_err(|_| "Invalid URL")?;
+    let start = std::time::Instant::now();
+
+    while start.elapsed() < TESTS_TIMEOUT {
+        let request_result =
+            agent.get(health_url.as_str()).send().await.map_err(|_| "Bad request")?;
+        match request_result.status() {
+            StatusCode::OK => return Ok(()),
+            StatusCode::NOT_FOUND => return Err("Endpoint not found"),
+            _ => std::thread::sleep(WAIT_SERVICE_INTERVAL),
+        }
+    }
+
+    Err("Timeout waiting for service to be ready")
+}

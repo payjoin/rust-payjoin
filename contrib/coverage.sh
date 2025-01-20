590295ae8a3d0bcf55f6a6148c218cd9138472b5
DanGould <d@ngould.dev>
2025-01-20 13:31:00 -0500

    Make `payjoin/v2` a default feature
    This marks Payjoin v2 (BIP 77) is a first-class citizen of the crate.

--- contrib/coverage.sh
+++ contrib/coverage.sh
@@ -3,6 +3,6 @@ set -e
 
 # https://github.com/taiki-e/cargo-llvm-cov?tab=readme-ov-file#merge-coverages-generated-under-different-test-conditions
 cargo llvm-cov clean --workspace # remove artifacts that may affect the coverage results
-cargo llvm-cov --no-report --features=v1
-cargo llvm-cov --no-report --features=v2,_danger-local-https,io
+cargo llvm-cov --no-report --no-default-features --features=v1
+cargo llvm-cov --no-report --no-default-features --features=v2,_danger-local-https,io
 cargo llvm-cov report --lcov --output-path lcov.info # generate report without tests

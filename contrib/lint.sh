9b9ee814bfb527ffee12ca5c1309b0317fd0c66b
DanGould <d@ngould.dev>
2025-01-20 14:21:05 -0500

    Configure v1 checks to use `_danger-local-https`
    Without this configuration some checks would be ignored.
    
    Close #499

--- contrib/lint.sh
+++ contrib/lint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-cargo clippy --all-targets --keep-going --no-default-features --features=v1 -- -D warnings
+cargo clippy --all-targets --keep-going --no-default-features --features=v1,_danger-local-https -- -D warnings
 cargo clippy --all-targets --keep-going --no-default-features --features=v2,_danger-local-https,io -- -D warnings

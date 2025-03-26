LtbLightning/payjoin-ffi

Expose uri error types
The inner error types are complex so they are first converted to strings
and the wrapper types implement From<String>.

src/uri/error.rs
@@ -1,0 +1,38 @@
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("Error parsing the payjoin URI: {message}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
+pub struct PjParseError {
+    message: String,
+}
+
+impl From<String> for PjParseError {
+    fn from(message: String) -> Self {
+        PjParseError { message }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("URI doesn't support payjoin: {message}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
+pub struct PjNotSupported {
+    message: String,
+}
+
+impl From<String> for PjNotSupported {
+    fn from(message: String) -> Self {
+        PjNotSupported { message }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("Error parsing URL: {message}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
+pub struct UrlParseError {
+    message: String,
+}
+
+impl From<payjoin::ParseError> for UrlParseError {
+    fn from(value: payjoin::ParseError) -> Self {
+        UrlParseError { message: format!("{:?}", value) }
+    }
+}

payjoin/rust-payjoin

src/uri/error.rs
@@ -1,0 +1,38 @@
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("Error parsing the payjoin URI: {msg}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Object))]
+pub struct PjParseError {
+    msg: String,
+}
+
+impl From<String> for PjParseError {
+    fn from(msg: String) -> Self {
+        PjParseError { msg }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("URI doesn't support payjoin: {msg}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Object))]
+pub struct PjNotSupported {
+    msg: String,
+}
+
+impl From<String> for PjNotSupported {
+    fn from(msg: String) -> Self {
+        PjNotSupported { msg }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("Error parsing URL: {msg}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Object))]
+pub struct UrlParseError {
+    msg: String,
+}
+
+impl From<payjoin::ParseError> for UrlParseError {
+    fn from(value: payjoin::ParseError) -> Self {
+        UrlParseError { msg: format!("{:?}", value) }
+    }
+}
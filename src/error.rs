LtbLightning/payjoin-ffi

Add SerdeJsonError mapping
Handle error conversions for `to_json` and `from_json` helpers.

src/error.rs
@@ -199,3 +199,15 @@ impl From<EncapsulationError> for PayjoinError {
         PayjoinError::EncapsulationError { message: format!("{:?}", value) }
     }
 }
+
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("Error de/serializing JSON object: {message}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
+pub struct SerdeJsonError {
+    message: String,
+}
+impl From<serde_json::Error> for SerdeJsonError {
+    fn from(value: serde_json::Error) -> Self {
+        SerdeJsonError { message: format!("{:?}", value) }
+    }
+}

payjoin/rust-payjoin

src/error.rs
@@ -199,3 +199,15 @@ impl From<EncapsulationError> for PayjoinError {
         PayjoinError::EncapsulationError { msg: format!("{:?}", value) }
     }
 }
+
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("Error de/serializing JSON object: {msg}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Object))]
+pub struct SerdeJsonError {
+    msg: String,
+}
+impl From<serde_json::Error> for SerdeJsonError {
+    fn from(value: serde_json::Error) -> Self {
+        SerdeJsonError { msg: format!("{:?}", value) }
+    }
+}
09848822d03d48085f34871d9b861b9a656bfdb5
Armin Sabouri <armins88@gmail.com>
2025-04-09 21:20:36 -0400

    Move persistence logic into separate module file
    Move persistence-related code out of `v2::Sender` and `v2::Receiver`
    into dedicated `persist.rs` files. This refactor helps declutter the
    core API and separates unrelated concerns.
    
    This change addresses a follow-up item from the [PR that introduced
    persistence](https://github.com/payjoin/rust-payjoin/pull/552#discussion_r2020220139).

--- payjoin/src/send/v2/mod.rs
+++ payjoin/src/send/v2/mod.rs
@@ -21,12 +21,11 @@
 //! [`bitmask-core`](https://github.com/diba-io/bitmask-core) BDK integration. Bring your own
 //! wallet and http client.
 
-use std::fmt::{self, Display};
-
 use bitcoin::hashes::{sha256, Hash};
 pub use error::{CreateRequestError, EncapsulationError, ImplementationError};
 use error::{InternalCreateRequestError, InternalEncapsulationError};
 use ohttp::ClientResponse;
+pub use persist::SenderToken;
 use serde::{Deserialize, Serialize};
 use url::Url;
 
@@ -34,12 +33,12 @@ use super::error::BuildSenderError;
 use super::*;
 use crate::hpke::{decrypt_message_b, encrypt_message_a, HpkeSecretKey};
 use crate::ohttp::{ohttp_decapsulate, ohttp_encapsulate};
-use crate::persist::{Persister, Value};
 use crate::send::v1;
 use crate::uri::{ShortId, UrlExt};
 use crate::{HpkeKeyPair, HpkePublicKey, IntoUrl, OhttpKeys, PjUri, Request};
 
 mod error;
+mod persist;
 
 /// A builder to construct the properties of a [`Sender`].
 #[derive(Clone)]
@@ -131,17 +130,6 @@ pub struct NewSender {
     pub(crate) reply_key: HpkeSecretKey,
 }
 
-impl NewSender {
-    /// Saves the new [`Sender`] using the provided persister and returns the storage token.
-    pub fn persist<P: Persister<Sender>>(
-        &self,
-        persister: &mut P,
-    ) -> Result<P::Token, ImplementationError> {
-        let sender = Sender { v1: self.v1.clone(), reply_key: self.reply_key.clone() };
-        Ok(persister.save(sender)?)
-    }
-}
-
 /// A payjoin V2 sender, allowing the construction of a payjoin V2 request
 /// and the resulting [`V2PostContext`].
 #[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -152,36 +140,7 @@ pub struct Sender {
     pub(crate) reply_key: HpkeSecretKey,
 }
 
-/// Opaque key type for the sender
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct SenderToken(Url);
-
-impl Display for SenderToken {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "{}", self.0) }
-}
-
-impl From<Sender> for SenderToken {
-    fn from(sender: Sender) -> Self { SenderToken(sender.endpoint().clone()) }
-}
-
-impl AsRef<[u8]> for SenderToken {
-    fn as_ref(&self) -> &[u8] { self.0.as_str().as_bytes() }
-}
-
-impl Value for Sender {
-    type Key = SenderToken;
-
-    fn key(&self) -> Self::Key { SenderToken(self.endpoint().clone()) }
-}
-
 impl Sender {
-    /// Loads a [`Sender`] from the provided persister using the storage token.
-    pub fn load<P: Persister<Sender>>(
-        token: P::Token,
-        persister: &P,
-    ) -> Result<Self, ImplementationError> {
-        persister.load(token).map_err(ImplementationError::from)
-    }
     /// Extract serialized V1 Request and Context from a Payjoin Proposal
     pub fn extract_v1(&self) -> (Request, v1::V1Context) { self.v1.extract_v1() }
 
---
+++ payjoin/src/send/v2/persist.rs
@@ -0,0 +1,48 @@
+use std::fmt::{self, Display};
+
+use url::Url;
+
+use super::{ImplementationError, NewSender, Sender};
+use crate::persist::{Persister, Value};
+impl NewSender {
+    /// Saves the new [`Sender`] using the provided persister and returns the storage token.
+    pub fn persist<P: Persister<Sender>>(
+        &self,
+        persister: &mut P,
+    ) -> Result<P::Token, ImplementationError> {
+        let sender = Sender { v1: self.v1.clone(), reply_key: self.reply_key.clone() };
+        Ok(persister.save(sender)?)
+    }
+}
+
+/// Opaque key type for the sender
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SenderToken(Url);
+
+impl Display for SenderToken {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "{}", self.0) }
+}
+
+impl From<Sender> for SenderToken {
+    fn from(sender: Sender) -> Self { SenderToken(sender.endpoint().clone()) }
+}
+
+impl AsRef<[u8]> for SenderToken {
+    fn as_ref(&self) -> &[u8] { self.0.as_str().as_bytes() }
+}
+
+impl Value for Sender {
+    type Key = SenderToken;
+
+    fn key(&self) -> Self::Key { SenderToken(self.endpoint().clone()) }
+}
+
+impl Sender {
+    /// Loads a [`Sender`] from the provided persister using the storage token.
+    pub fn load<P: Persister<Sender>>(
+        token: P::Token,
+        persister: &P,
+    ) -> Result<Self, ImplementationError> {
+        persister.load(token).map_err(ImplementationError::from)
+    }
+}

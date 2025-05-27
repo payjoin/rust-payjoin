9719ea44f8334ab8cd636387abaaa138d54d1499
user <benalleng@gmail.com>
2025-05-27 12:13:48 -0400

    Remove all features llvm coverage check

--- contrib/coverage.sh
+++ contrib/coverage.sh
@@ -3,6 +3,6 @@ set -e
 
 # https://github.com/taiki-e/cargo-llvm-cov?tab=readme-ov-file#merge-coverages-generated-under-different-test-conditions
 cargo llvm-cov clean --workspace # remove artifacts that may affect the coverage results
-cargo llvm-cov --no-report --all-features
-cargo llvm-cov --no-report --package payjoin-cli --no-default-features --features=v1,_danger-local-https # Explicitly run payjoin-cli v1 e2e tests
+cargo llvm-cov --no-report --no-default-features --features=v1,v2,io,_multiparty
+cargo llvm-cov --no-report --package payjoin-cli --no-default-features --features=v1 # Explicitly run payjoin-cli v1 e2e tests
 cargo llvm-cov report --lcov --output-path lcov.info # generate report without tests

fb386b4e5e8b700858c9a99ef18e837c0cd1708e
spacebear <git@spacebear.dev>
2025-06-12 16:54:49 -0400

    Initialize payjoin_dart

--- payjoin-ffi/uniffi-bindgen.rs
+++ payjoin-ffi/uniffi-bindgen.rs
@@ -1,4 +1,13 @@
 fn main() {
     #[cfg(feature = "uniffi")]
-    uniffi::uniffi_bindgen_main()
+    uniffi::uniffi_bindgen_main();
+    #[cfg(feature = "uniffi")]
+    uniffi_dart::gen::generate_dart_bindings(
+        "src/payjoin_ffi.udl".into(),
+        None,
+        Some("dart/lib".into()),
+        "target/release/libpayjoin_ffi.dylib".into(),
+        true,
+    )
+    .unwrap();
 }

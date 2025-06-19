c2d2eae97f9e2d659b1de82d131280a924f7cc6c
user <benalleng@gmail.com>
2025-06-19 12:13:20 -0400

    Specify the language to create bindings at the bindings creation step
    I've included support for including --language args beyond those explicitly
    supported in uniffi-rs with a match block that passes the chosen language
    to chose what binding cargo package to use.

--- payjoin-ffi/uniffi-bindgen.rs
+++ payjoin-ffi/uniffi-bindgen.rs
@@ -1,13 +1,35 @@
 fn main() {
     #[cfg(feature = "uniffi")]
-    uniffi::uniffi_bindgen_main();
-    #[cfg(feature = "uniffi")]
-    uniffi_dart::gen::generate_dart_bindings(
-        "src/payjoin_ffi.udl".into(),
-        None,
-        Some("dart/lib".into()),
-        "target/release/libpayjoin_ffi.dylib".into(),
-        true,
-    )
-    .unwrap();
+    uniffi_bindgen()
+}
+
+#[cfg(feature = "uniffi")]
+fn uniffi_bindgen() {
+    use std::env;
+    let args: Vec<String> = env::args().collect();
+    let language =
+        args.iter().position(|arg| arg == "--language").and_then(|idx| args.get(idx + 1));
+    let library_path = args
+        .iter()
+        .position(|arg| arg == "--library")
+        .and_then(|idx| args.get(idx + 1))
+        .expect("specify the library path with --library");
+    let output_dir = args
+        .iter()
+        .position(|arg| arg == "--out-dir")
+        .and_then(|idx| args.get(idx + 1))
+        .expect("--out-dir is required when using --library");
+    match language {
+        Some(lang) if lang == "dart" => {
+            uniffi_dart::gen::generate_dart_bindings(
+                "src/payjoin_ffi.udl".into(),
+                None,
+                Some(output_dir.as_str().into()),
+                library_path.as_str().into(),
+                true,
+            )
+            .expect("Failed to generate dart bindings");
+        }
+        _ => uniffi::uniffi_bindgen_main(),
+    }
 }

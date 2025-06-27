94e36430cc5835129fbd3aec73d53236b61935e3
user <benalleng@gmail.com>
2025-06-27 15:11:30 -0400

    Add persistence unit tests

--- payjoin-ffi/dart/test/test_payjoin_unit_test.dart
+++ payjoin-ffi/dart/test/test_payjoin_unit_test.dart
@@ -1,7 +1,57 @@
 import 'package:test/test.dart';
 import "../lib/payjoin_ffi.dart" as payjoin;
+import "../lib/bitcoin.dart" as bitcoin;
+
+class InMemoryReceiverPersister
+    implements payjoin.JsonReceiverSessionPersister {
+  final String id;
+  final List<String> events = [];
+  bool closed = false;
+
+  InMemoryReceiverPersister(this.id);
+
+  @override
+  void save(String event) {
+    events.add(event);
+  }
+
+  @override
+  List<String> load() {
+    return events;
+  }
+
+  @override
+  void close() {
+    closed = true;
+  }
+}
+
+class InMemorySenderPersister implements payjoin.JsonSenderSessionPersister {
+  final String id;
+  final List<String> events = [];
+  bool closed = false;
+
+  InMemorySenderPersister(this.id);
+
+  @override
+  void save(String event) {
+    events.add(event);
+  }
+
+  @override
+  List<String> load() {
+    return events;
+  }
+
+  @override
+  void close() {
+    closed = true;
+  }
+}
 
 void main() {
+  payjoin.initialize();
+  payjoin.ensureInitialized();
   group('Test URIs', () {
     test('Test todo url encoded', () {
       var uri =
@@ -26,8 +76,7 @@ void main() {
     });
 
     test('Test valid uris', () {
-      // import test_utils to allow for const EXAMPLE_URL
-      final https = "https://example.com";
+      final https = payjoin.exampleUrl();
       final onion =
           "http://vjdpwgybvubne5hda6v4c5iaeeevhge6jvo3w2cl6eocbwwvwxp7b7qd.onion";
 
@@ -48,6 +97,49 @@ void main() {
           }
         }
       }
+    });
+  });
+
+  group("Test Persistence", () {
+    test("Test receiver persistence", () {
+      var persister = InMemoryReceiverPersister("1");
+      var address = bitcoin.Address(
+          "tb1q6d3a2w975yny0asuvd9a67ner4nks58ff0q8g4", bitcoin.Network.signet);
+      payjoin.UninitializedReceiver()
+          .createSession(
+              address,
+              "https://example.com",
+              payjoin.OhttpKeys.fromString(
+                  "OH1QYPM5JXYNS754Y4R45QWE336QFX6ZR8DQGVQCULVZTV20TFVEYDMFQC"),
+              null)
+          .save(persister);
+      final result = payjoin.replayReceiverEventLog(persister);
+      expect(result, isA<payjoin.ReplayResult>(),
+          reason: "persistence should return a replay result");
+    });
+
+    test("Test sender persistence", () {
+      var receiver_persister = InMemoryReceiverPersister("1");
+      var address = bitcoin.Address(
+          "2MuyMrZHkbHbfjudmKUy45dU4P17pjG2szK", bitcoin.Network.testnet);
+      var receiver = payjoin.UninitializedReceiver()
+          .createSession(
+              address,
+              "https://example.com",
+              payjoin.OhttpKeys.fromString(
+                  "OH1QYPM5JXYNS754Y4R45QWE336QFX6ZR8DQGVQCULVZTV20TFVEYDMFQC"),
+              null)
+          .save(receiver_persister);
+      var uri = receiver.pjUri();
+
+      var sender_persister = InMemorySenderPersister("1");
+      var psbt =
+          "cHNidP8BAHMCAAAAAY8nutGgJdyYGXWiBEb45Hoe9lWGbkxh/6bNiOJdCDuDAAAAAAD+////AtyVuAUAAAAAF6kUHehJ8GnSdBUOOv6ujXLrWmsJRDCHgIQeAAAAAAAXqRR3QJbbz0hnQ8IvQ0fptGn+votneofTAAAAAAEBIKgb1wUAAAAAF6kU3k4ekGHKWRNbA1rV5tR5kEVDVNCHAQcXFgAUx4pFclNVgo1WWAdN1SYNX8tphTABCGsCRzBEAiB8Q+A6dep+Rz92vhy26lT0AjZn4PRLi8Bf9qoB/CMk0wIgP/Rj2PWZ3gEjUkTlhDRNAQ0gXwTO7t9n+V14pZ6oljUBIQMVmsAaoNWHVMS02LfTSe0e388LNitPa1UQZyOihY+FFgABABYAFEb2Giu6c4KO5YW0pfw3lGp9jMUUAAA=";
+      final result = payjoin.SenderBuilder(psbt, uri)
+          .buildRecommended(1000)
+          .save(sender_persister);
+      expect(result, isA<payjoin.WithReplyKey>(),
+          reason: "persistence should return a reply key");
     });
   });
 }

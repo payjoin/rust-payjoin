b1f803f194f62ca28b998f15b6af1ae144d2302b
user <benalleng@gmail.com>
2025-09-03 15:27:58 -0400

    Add test suite for v1 and v2 specific features alone
    Add the test suite for the `contrib/test,sh` script in the payjoin
    crate.

--- payjoin/contrib/test.sh
+++ payjoin/contrib/test.sh
@@ -3,3 +3,9 @@ set -e
 
 cargo test --locked --package payjoin --verbose --all-features --lib
 cargo test --locked --package payjoin --verbose --all-features --test integration
+
+cargo test --locked --package payjoin --verbose --no-default-features --features v1 --lib
+cargo test --locked --package payjoin --verbose --no-default-features --features v1 --test integration
+
+cargo test --locked --package payjoin --verbose --no-default-features --features v2 --lib
+cargo test --locked --package payjoin --verbose --no-default-features --features v2 --test integration

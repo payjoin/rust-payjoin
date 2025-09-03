76f21536f03eb3df9a6b7e90a44a635b399bcec5
user <benalleng@gmail.com>
2025-09-03 15:53:38 -0400

    Feature gate integration tests
    This commit locks the integration tests down into their specific enabled
    versions.

--- payjoin/contrib/test.sh
+++ payjoin/contrib/test.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+features=("v1" "v2")
+
 cargo test --locked --package payjoin --verbose --all-features --lib
 cargo test --locked --package payjoin --verbose --all-features --test integration
+
+for feature in "${features[@]}"; do
+    cargo test --locked --package payjoin --verbose --no-default-features --features "$feature" --lib
+    cargo test --locked --package payjoin --verbose --no-default-features --features "$feature" --test integration
+done

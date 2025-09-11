4c9d7ea1c6493c84b6f0bc37a12d1b047b97889f
spacebear <git@spacebear.dev>
2025-09-10 21:28:02 -0400

    Introduce HasReplyableError typestate
    This enforces proper handling within the receiver state machine, such
    that they must attempt replying to the sender with an error response
    before the session can be considered closed.
    
    Co-authored-by: DanGould <d@ngould.dev>

--- payjoin-cli/src/app/v2/mod.rs
+++ payjoin-cli/src/app/v2/mod.rs
@@ -6,9 +6,9 @@ use payjoin::bitcoin::consensus::encode::serialize_hex;
 use payjoin::bitcoin::{Amount, FeeRate};
 use payjoin::persist::OptionalTransitionOutcome;
 use payjoin::receive::v2::{
-    process_err_res, replay_event_log as replay_receiver_event_log, Initialized, MaybeInputsOwned,
-    MaybeInputsSeen, OutputsUnknown, PayjoinProposal, ProvisionalProposal, ReceiveSession,
-    Receiver, ReceiverBuilder, SessionHistory, UncheckedOriginalPayload, WantsFeeRange,
+    replay_event_log as replay_receiver_event_log, HasReplyableError, Initialized,
+    MaybeInputsOwned, MaybeInputsSeen, OutputsUnknown, PayjoinProposal, ProvisionalProposal,
+    ReceiveSession, Receiver, ReceiverBuilder, UncheckedOriginalPayload, WantsFeeRange,
     WantsInputs, WantsOutputs,
 };
 use payjoin::send::v2::{
@@ -70,6 +70,7 @@ impl StatusText for ReceiveSession {
             | ReceiveSession::WantsFeeRange(_)
             | ReceiveSession::ProvisionalProposal(_) => "Processing original proposal",
             ReceiveSession::PayjoinProposal(_) => "Payjoin proposal sent",
+            ReceiveSession::HasReplyableError(_) => "Session failure",
             ReceiveSession::TerminalFailure => "Session failure",
         }
     }
@@ -521,22 +522,13 @@ impl App {
                     self.finalize_proposal(proposal, persister).await,
                 ReceiveSession::PayjoinProposal(proposal) =>
                     self.send_payjoin_proposal(proposal, persister).await,
+                ReceiveSession::HasReplyableError(error) =>
+                    self.handle_error(error, persister).await,
                 ReceiveSession::TerminalFailure =>
                     return Err(anyhow!("Terminal receiver session")),
             }
         };
-
-        match res {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                let (_, session_history) = replay_receiver_event_log(persister)?;
-                let pj_uri = session_history.pj_uri().extras.endpoint().clone();
-                let ohttp_relay = self.unwrap_relay_or_else_fetch(Some(pj_uri)).await?;
-                self.handle_recoverable_error(&ohttp_relay, &session_history).await?;
-
-                Err(e)
-            }
-        }
+        res
     }
 
     #[allow(clippy::incompatible_msrv)]
@@ -702,20 +694,14 @@ impl App {
         Ok(ohttp_relay)
     }
 
-    /// Handle request error by sending an error response over the directory
-    async fn handle_recoverable_error(
+    /// Handle error by attempting to send an error response over the directory
+    async fn handle_error(
         &self,
-        ohttp_relay: &payjoin::Url,
-        session_history: &SessionHistory,
+        session: Receiver<HasReplyableError>,
+        persister: &ReceiverPersister,
     ) -> Result<()> {
-        let e = match session_history.terminal_error() {
-            Some(e) => e,
-            _ => return Ok(()),
-        };
-        let (err_req, err_ctx) = session_history
-            .extract_err_req(ohttp_relay.as_str())?
-            .expect("If JsonReply is Some, then err_req and err_ctx should be Some");
-        let to_return = anyhow!("Replied with error: {}", e.to_json());
+        let (err_req, err_ctx) =
+            session.create_error_request(self.unwrap_relay_or_else_fetch(None).await?.as_str())?;
 
         let err_response = match self.post_request(err_req).await {
             Ok(response) => response,
@@ -727,11 +713,11 @@ impl App {
             Err(e) => return Err(anyhow!("Failed to get error response bytes: {}", e)),
         };
 
-        if let Err(e) = process_err_res(&err_bytes, err_ctx) {
+        if let Err(e) = session.process_error_response(&err_bytes, err_ctx).save(persister) {
             return Err(anyhow!("Failed to process error response: {}", e));
         }
 
-        Err(to_return)
+        Ok(())
     }
 
     async fn post_request(&self, req: payjoin::Request) -> Result<reqwest::Response> {

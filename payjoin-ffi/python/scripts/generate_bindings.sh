038d9bdcc1e39f8e1ddebd48ee7f5a519e0bfdb0
Brandon Lucas <blu@blu.cx>
2025-09-14 14:09:49 -0400

    add uv to generate_bindings.sh and FIXMEs/TODOs

--- payjoin-ffi/python/scripts/generate_bindings.sh
+++ payjoin-ffi/python/scripts/generate_bindings.sh
@@ -5,50 +5,58 @@ OS=$(uname -s)
 
 echo "Running on $OS"
 
+# TODO: is this necessary if we're reliant on nix for the env?
+# Or should we keep this and make nix optional?
+# FIXME: pin the versions between the darwin and linux
+
 # Install Rust targets if on macOS
 if [[ "$OS" == "Darwin" ]]; then
-    LIBNAME=libpayjoin_ffi.dylib
-    python3 --version
-    pip install -r requirements.txt -r requirements-dev.txt
+  LIBNAME=libpayjoin_ffi.dylib
+  # FIXME: why does darwin not assume pybin has been set?
+  # are we just assuming the python3 binary is installed on the system?
+  uv python3 --version
+  uv pip install -r requirements.txt -r requirements-dev.txt
 elif [[ "$OS" == "Linux" ]]; then
-    # sudo apt update
-    # sudo apt install -y build-essential python3-dev
-    LIBNAME=libpayjoin_ffi.so
-    PYBIN=$(dirname $(which python))
-    PYBIN="$PYBIN" 
-    ${PYBIN}/python --version
-    # ${PYBIN}/pip install -r requirements.txt -r requirements-dev.txt
+  # sudo apt update
+  # sudo apt install -y build-essential python3-dev
+  LIBNAME=libpayjoin_ffi.so
+  PYBIN=$(dirname $(which python))
+  PYBIN="$PYBIN"
+  ${PYBIN}/python --version
+  # ${PYBIN}/pip install -r requirements.txt -r requirements-dev.txt
 else
-    echo "Unsupported os: $OS"
-    exit 1
+  echo "Unsupported os: $OS"
+  exit 1
 fi
 
+# FIXME: should we not use pushd and popd to ensure robustness here
 cd ../
 # This is a test script the actual release should not include the test utils feature
-cargo build --features _test-utils --profile release 
+cargo build --features _test-utils --profile release
 cargo run --features _test-utils --profile release --bin uniffi-bindgen generate --library target/release/$LIBNAME --language python --out-dir python/src/payjoin/
 
 if [[ "$OS" == "Darwin" ]]; then
-    echo "Generating native binaries..."
-    rustup target add aarch64-apple-darwin x86_64-apple-darwin
-    # This is a test script the actual release should not include the test utils feature
-    cargo build --profile release-smaller --target aarch64-apple-darwin --features _test-utils &
-    cargo build --profile release-smaller --target x86_64-apple-darwin --features _test-utils &
-    wait
+  echo "Generating native binaries..."
+  rustup target add aarch64-apple-darwin x86_64-apple-darwin
+  # This is a test script the actual release should not include the test utils feature
+  cargo build --profile release-smaller --target aarch64-apple-darwin --features _test-utils &
+  cargo build --profile release-smaller --target x86_64-apple-darwin --features _test-utils &
+  wait
 
-    echo "Building macos fat library"
-    lipo -create -output python/src/payjoin/$LIBNAME \
-        target/aarch64-apple-darwin/release-smaller/$LIBNAME \
-        target/x86_64-apple-darwin/release-smaller/$LIBNAME
-
+  echo "Building macos fat library"
+  # NOTE: requires xcode?
+  lipo -create -output python/src/payjoin/$LIBNAME \
+    target/aarch64-apple-darwin/release-smaller/$LIBNAME \
+    target/x86_64-apple-darwin/release-smaller/$LIBNAME
 else
-    echo "Generating native binaries..."
-    # rustup target add x86_64-unknown-linux-gnu
-    # This is a test script the actual release should not include the test utils feature
-    cargo build --profile release-smaller --target x86_64-unknown-linux-gnu --features _test-utils
+  echo "Generating native binaries..."
+  # rustup target add x86_64-unknown-linux-gnu
+  # This is a test script the actual release should not include the test utils feature
+  cargo build --profile release-smaller --target x86_64-unknown-linux-gnu --features _test-utils
 
-    echo "Copying payjoin_ffi binary"
-    cp target/x86_64-unknown-linux-gnu/release-smaller/$LIBNAME python/src/payjoin/$LIBNAME
+  echo "Copying payjoin_ffi binary"
+  # FIXME: only works on x86_64 arch's?
+  cp target/x86_64-unknown-linux-gnu/release-smaller/$LIBNAME python/src/payjoin/$LIBNAME
 fi
 
 echo "All done!"

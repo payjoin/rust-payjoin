27e57663f50deb3bf76d17037a81eb92596dc9f0
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2025-09-20 22:53:39 +0300

    test: add uppercase assertion to bech32 test
    Complete the TODO comment by adding an assertion that verifies the
    encoded bech32 string is uppercase. The assertion checks that all
    characters are either ASCII uppercase letters or the separator '1'.
    
    This addresses the TODO comment in the bech32 test and improves
    test coverage by ensuring the encoding produces uppercase output.

--- payjoin/src/bech32.rs
+++ payjoin/src/bech32.rs
@@ -41,7 +41,8 @@ mod test {
             (hrp.as_str().len() + 1) as f32 + (bytes.len() as f32 * 8.0 / 5.0).ceil()
         );
 
-        // TODO assert uppercase
+        // assert uppercase
+        assert!(encoded.chars().all(|c| c.is_ascii_uppercase() || c == '1'));
 
         // should not error
         let corrupted = encoded + "QQPP";

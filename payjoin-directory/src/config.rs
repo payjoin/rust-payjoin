44e74b02ca7a6bccdbd820935e7cd71f19448af5
Yuval Kogman <nothingmuch@woobling.org>
2025-10-08 22:14:57 +0200

    don't override config values with cli defaults
    Makes all CLI args `Option<T>`, because unless explicitly provided, args
    shouldn't override config values.

--- payjoin-directory/src/config.rs
+++ payjoin-directory/src/config.rs
@@ -41,12 +41,22 @@ impl Config {
 
 fn add_defaults(config: Builder, cli: &Cli) -> Result<Builder, ConfigError> {
     config
-        .set_override_option("listen_addr", Some(format!("[::]:{}", cli.port)))?
+        .set_default("listen_addr", "[::]:8080")?
+        .set_override_option("listen_addr", cli.port.map(|port| format!("[::]:{}", port)))?
+        .set_default("metrics_listen_addr", Option::<String>::None)?
         .set_override_option(
             "metrics_listen_addr",
             cli.metrics_port.map(|port| format!("localhost:{}", port)),
         )?
-        .set_override_option("timeout", Some(cli.timeout))?
-        .set_override_option("ohttp_keys", Some(cli.ohttp_keys.to_string_lossy().into_owned()))?
-        .set_override_option("storage_dir", Some(cli.storage_dir.to_string_lossy().into_owned()))
+        .set_default("timeout", Some(30))?
+        .set_override_option("timeout", cli.timeout)?
+        .set_default("ohttp_keys", "ohttp_keys")?
+        .set_override_option(
+            "ohttp_keys",
+            cli.ohttp_keys.clone().map(|s| s.to_string_lossy().into_owned()),
+        )?
+        .set_override_option(
+            "storage_dir",
+            cli.storage_dir.clone().map(|s| s.to_string_lossy().into_owned()),
+        )
 }

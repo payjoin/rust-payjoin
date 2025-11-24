3e69a79506e89495dc92b3557afae36be9887090
spacebear <git@spacebear.dev>
2025-11-24 11:45:59 -0500

    Switch to Dart native assets
    With the changes from
    https://github.com/Uniffi-Dart/uniffi-dart/pull/99, the native libraries
    can be compiled automagically in a dart build hook.

---
+++ payjoin-ffi/dart/native/src/lib.rs
@@ -0,0 +1,1 @@
+pub use payjoin_ffi::*;
--- payjoin-ffi/uniffi-bindgen.rs
+++ payjoin-ffi/uniffi-bindgen.rs
@@ -21,7 +21,7 @@ fn uniffi_bindgen() {
                 .expect("--out-dir is required when using --library");
             uniffi_dart::gen::generate_dart_bindings(
                 "src/payjoin.udl".into(),
-                None,
+                Some("uniffi.toml".into()),
                 Some(output_dir.as_str().into()),
                 library_path.as_str().into(),
                 true,

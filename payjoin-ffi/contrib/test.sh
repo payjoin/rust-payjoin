aefa6e46725c5ba14f47fa2dc067960146037754
carlos santos <rickhenriquetab@gmail.com>
2026-04-25 16:30:54 -0300

    fix(test): parallelize FFI language binding tests in contrib/test.sh

--- payjoin-ffi/contrib/test.sh
+++ payjoin-ffi/contrib/test.sh
@@ -7,9 +7,18 @@ cargo test --package payjoin-ffi --verbose --features=_manual-tls,_test-utils
 
 BINDINGS="dart javascript python csharp"
 
+pids=()
 for binding in $BINDINGS; do
     (
         cd "$binding"
         ./contrib/test.sh
-    )
+    ) &
+    pids+=($!)
 done
+
+failed=0
+for pid in "${pids[@]}"; do
+    wait "$pid" || failed=1
+done
+
+exit $failed
